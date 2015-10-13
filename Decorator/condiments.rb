4745f83635f53c6d98d6d7cec8a36283f49bc834
Karan Valecha <karan.valecha92@gmail.com>
2015-10-14 00:49:51 +0530

    Add condiments modules

---
+++ Decorator/condiments.rb
@@ -0,0 +1,22 @@
+module Condiment
+  module SteamedMilk
+    def cost
+      super()+0.10
+    end
+  end
+  module SoyMilk 
+    def cost
+      super()+0.15
+    end
+  end
+  module Mocha
+    def cost
+      super()+0.20
+    end
+  end
+  module WhipCream
+    def cost
+      super()+0.10
+    end
+  end
+end
