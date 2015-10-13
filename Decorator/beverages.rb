edce6c2da9075f2b1cfa3256892125d0aa15693d
Karan Valecha <karan.valecha92@gmail.com>
2015-10-14 00:49:07 +0530

    Add the beverage module

---
+++ Decorator/beverages.rb
@@ -0,0 +1,22 @@
+module Beverage
+  class HouseBlend
+    def cost
+      0.89
+    end
+  end
+  class DarkRoast
+    def cost
+      0.99
+    end
+  end
+  class Decaf
+    def cost
+      1.05
+    end
+  end
+  class Espresso
+    def cost
+      1.99
+    end
+  end
+end
